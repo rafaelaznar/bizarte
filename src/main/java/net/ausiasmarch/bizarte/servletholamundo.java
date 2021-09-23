ffd2db0a12eef4d84b4a0a07890b4ae2240ae8f3
RAFAEL AZNAR <rafaaznar@gmail.com>
2021-09-23 10:00:38 +0200

    commit inicial

---
+++ src/main/java/net/ausiasmarch/bizarte/servletholamundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.ausiasmarch.bizarte;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author raznara
+ */
+public class servletholamundo extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.print("<html><body>HOLA MUNDO</body></html>");
+        }
+    }
+}
