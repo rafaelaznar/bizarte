2fa58f0e918e2d7ee77b24b5e4a28dd66deb0060
Rafael Aznar <rafa.aznar@gmail.com>
2022-02-15 14:52:20 +0100

    mejoras

--- src/main/java/net/ausiasmarch/bizarte/holamundo.java
+++ src/main/java/net/ausiasmarch/bizarte/holamundo.java
@@ -11,16 +11,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author raznara
- */
-public class servletholamundo extends HttpServlet {
+public class holamundo extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            out.print("<html><body>HOLA MUNDO</body></html>");
+            out.print("<html><body>");
+            out.print("<h1>Servlet HOLA MUNDO: observa el fuente en una sola línea!</h1>");
+            out.print("<a href=\"control\">Ir al servlet control</a><br>");
+            out.print("<a href=\"index.html\">Ir a la página index.html</a>");
+            out.print("</body></html>");
         }
     }
 }
